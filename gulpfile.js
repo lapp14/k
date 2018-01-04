e9a3fce9c3ab368ed6dd2b0dba94a32af3931aa2
dan <d.lapp14@gmail.com>
2018-01-04 15:45:19 -0500

    navbar work

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('serve', ['sass'], function() {
         server: "./src"  
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
